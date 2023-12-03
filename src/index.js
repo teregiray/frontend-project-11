8866264ad2044a1f4941e015b417df7e48ffc996
teregiray <dantonengier@mail.ru>
2023-12-03 11:18:12 +0300

    add validation

--- src/index.js
+++ src/index.js
@@ -4,29 +4,67 @@ import * as yup from 'yup';
 
 const state = {
   link: null,
-  valid: false,
+  valid: {
+    rules: null,
+    duplicate: null,
+  },
+  feeds: [],
 };
 
-const form = document.querySelector('form'); //eslint-disable-line
+const form = document.querySelector("form"); // eslint-disable-line
+const urlInput = form.querySelector("#url-input"); // eslint-disable-line
+const feedback = document.querySelector(".feedback"); // eslint-disable-line
+
+const render = () => {
+  urlInput.value = '';
+  urlInput.focus();
+
+  if (state.valid.duplicate) {
+    feedback.classList.remove('text-success');
+    feedback.classList.add('text-danger');
+    feedback.textContent = 'RSS уже существует';
+  } else if (state.valid.rules) {
+    urlInput.classList.remove('is-invalid');
+    feedback.classList.remove('text-danger');
+    feedback.classList.add('text-success');
+    feedback.textContent = 'RSS успешно загружен';
+  } else {
+    urlInput.classList.add('is-invalid');
+    feedback.classList.remove('text-success');
+    feedback.classList.add('text-danger');
+    feedback.textContent = 'Ссылка должна быть валидным URL';
+  }
+};
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
 
-  const inputLink = form.querySelector('#url-input').value;
+  const inputLink = urlInput.value;
   state.link = inputLink;
 
+  // дубли
+  if (state.feeds.includes(inputLink)) {
+    state.valid.duplicate = true;
+    render();
+    return;
+  }
+
+  // валидация
   const schema = yup.object().shape({
-    link: yup.string().required(),
+    link: yup.string().url().required(),
   });
 
   try {
     await schema.validate(state);
-    console.log(`success: ${state.link}`); // remove aft
-    state.valid = true;
+    state.valid.rules = true;
+    state.feeds.push(inputLink);
   } catch (err) {
-    console.log(`error: ${err.errors}`); // remove aft
-    state.valid = false;
+    // console.log(`error: ${err.errors}`);
+    state.valid.rules = false;
   }
 
-  console.log(state.valid); // remove aft
+  render();
+  console.log(state);
 });
+
+urlInput.focus();
